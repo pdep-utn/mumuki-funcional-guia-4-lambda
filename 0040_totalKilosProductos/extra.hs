e5f34c2a55ede325e3a318635add9f41b27ca14f
iluetich <ivan.luetich93@gmail.com>
2015-04-16 03:41:17 -0300

    Fix de comillas

--- 0040_totalKilosProductos/extra.hs
+++ 0040_totalKilosProductos/extra.hs
@@ -1,1 +1,1 @@
-productos = [(“manzana”, 50), (“banana”, 30), (“naranja”, 40), (“papa”, 40), (“tomate”, 25)] 
+productos = [("manzana", 50), ("banana", 30), ("naranja", 40), ("papa", 40), ("tomate", 25)] 
