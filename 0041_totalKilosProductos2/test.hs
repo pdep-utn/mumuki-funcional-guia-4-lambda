f7199bfb0a946cb9b9a9a7c602b6d3ec225c466c
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-04-18 16:21:52 -0300

    Corrigiendo typo

--- 0041_totalKilosProductos2/test.hs
+++ 0041_totalKilosProductos2/test.hs
@@ -1,14 +1,14 @@
-describe "totalKilosProducto" $ do
+describe "totalKilosProductos" $ do
   let productos = [("manzana", 50), ("banana", 30), ("naranja", 40), ("papa", 40), ("tomate", 25)]
 
-  it "totalKilosProducto productos == 185" $ do
-    totalKilosProducto productos `shouldBe` 185
+  it "totalKilosProductos productos == 185" $ do
+    totalKilosProductos productos `shouldBe` 185
 
-  it "totalKilosProducto [] == 0" $ do
-    totalKilosProducto [] `shouldBe` 0
+  it "totalKilosProductos [] == 0" $ do
+    totalKilosProductos [] `shouldBe` 0
 
-  it "totalKilosProducto [(\"melon\", 10)] == 0" $ do
-    totalKilosProducto [("melon", 10)] `shouldBe` 10
+  it "totalKilosProductos [(\"melon\", 10)] == 0" $ do
+    totalKilosProductos [("melon", 10)] `shouldBe` 10
 
-  it "totalKilosProducto [(\"melon\", 2), (\"sandia\", 20)] == 0" $ do
-    totalKilosProducto [("melon", 2), ("sandia", 20)] `shouldBe` 22
+  it "totalKilosProductos [(\"melon\", 2), (\"sandia\", 20)] == 0" $ do
+    totalKilosProductos [("melon", 2), ("sandia", 20)] `shouldBe` 22
